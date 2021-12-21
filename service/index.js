d493d6f5eb964d02c7acaadea7fd2aa0b73b10d5
Daniel Atonge <elamboatonge@gmail.com>
2021-12-21 17:22:26 +0300

    configs for deployment

--- service/index.js
+++ service/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: `http://localhost:3001`,
+    baseURL: `https://my-json-server.typicode.com/Danielatonge/Nuxt-refresher/`,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
