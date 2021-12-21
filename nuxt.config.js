a8dafaae5afd3df80d892e784f819ab28af37014
Daniel Atonge <elamboatonge@gmail.com>
2021-12-21 14:39:32 +0300

    seo with vue-meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,16 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'real-worl-nuxt',
+    titleTemplate: '%s - Real World Events',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Where you can find all the events taking place in your neighborhood',
+      },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Create an Event</h1>
+</template>
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create events taking place in your neighborhood',
+        },
+      ],
+    }
+  },
+}
+</script>
